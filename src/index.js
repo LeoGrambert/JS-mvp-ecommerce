3463ef79b771fd18b69666feee22ebe1e385402e
Léo Grambert <leogrambert+1@protonmail.com>
2021-08-27 17:05:33 +0200

    feat: add form for order

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import products from './components/products';
 import product from './components/product';
 import cart from './components/cart';
+import form from './components/form';
 import order from './components/order';
 import { displayBadgeCart } from './utils/helpers';
 
@@ -14,6 +15,7 @@ const main = async () => {
       break;
     case '/pages/cart.html':
       await cart();
+      await form();
       break;
     case '/pages/order.html':
       await order();
@@ -26,8 +28,6 @@ const main = async () => {
 main();
 displayBadgeCart();
 
-// TODO Formulaire de paiement
-// TODO stripe
 // TODO POST request & localstorage order
 // TODO display order page
 // TODO validation front du formulaire
